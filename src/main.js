71562c740354950ba85e47efea9c2c61e88352d1
petrickad <petrick.duarte99@gmail.com>
2025-03-24 12:27:27 -0300

    exercicio es6

---
+++ src/main.js
@@ -0,0 +1,19 @@
+class Aluno{
+    nome = '';
+    nota = '';
+
+    constructor(nomeAluno, notaAluno){
+        this.nome = nomeAluno;
+        this.nota = notaAluno;
+    }
+}
+
+const alunos = [
+    new Aluno('Maria', 10),
+    new Aluno('Pedro', 4),
+    new Aluno('Ana', 7)
+]; 
+
+const alunosAprovados = alunos.filter(aluno => aluno.nota > 6);
+
+console.log(alunosAprovados); 
